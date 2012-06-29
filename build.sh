3c652e6c1f4916ede0b79f37d306115e69c284d8
Chris <chris@livefyre.com>
2012-06-28 17:27:17 -0700

    Adding a pretty echo to mention the construction of the zip file during build.

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 zip -r livefyre-`sed 's/\./_/' version`.zip livefyre-comments/ -x "livefyre-comments/**/.*" -x "livefyre-comments/.*"
+echo "
+*Made a new zip file for you: livefyre-`sed 's/\./_/' version`.zip"
