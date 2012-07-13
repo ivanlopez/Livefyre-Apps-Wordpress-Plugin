8263749306690b3d028cc93b2dfb69d2aac3d1c3
Chris <chris@livefyre.com>
2012-07-12 19:52:09 -0700

    Remove urldecode() filter from import request handler. Add missing trackback filter function as a static class method. Remove some 'import status' updates from the JS - these are mostly handled by the server-server calls as data is sent to Livefyre. Adjusted spacing on the loader bar. Added a pause before switch to 'completed' state - improves UX for very fast imports (in some cases it just snaps to 'complete' messaging without ever filling the bar.  Added a 'Completed On' message when the final import step is complete, and code to display it with the success status. Enabling ajax updater for other non-error states besides 'started' - otherwise fast-initializing imports can unintentionally skip showing the loading bar at all. Disabling ajax updater for 'csv_uploaded' aka completed state. Removing the example ajax updater payload.

--- livefyre-comments/settings-template.php
+++ livefyre-comments/settings-template.php
@@ -16,16 +16,25 @@ if (isset($_GET['status'])) {
 ?>
 
 <script type="text/javascript">
-/*{"status": "created", "conversations_processed_count": 0, "last_modified": "2011-03-01T07:54:31", "aborted": false, "messages": "2011-03-01 07:54:31.910405\nCreated\n--------------------------------------------------\n", "conversation_count": 0, "import_queue": 1}*/
 //Lightweight JSONP fetcher - www.nonobtrusive.com
 var JSONP=(function(){var a=0,c,f,b,d=this;function e(j){var i=document.createElement("script"),h=false;i.src=j;i.async=true;i.onload=i.onreadystatechange=function(){if(!h&&(!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){h=true;i.onload=i.onreadystatechange=null;if(i&&i.parentNode){i.parentNode.removeChild(i)}}};if(!c){c=document.getElementsByTagName("head")[0]}c.appendChild(i)}function g(h,j,k){f="?";j=j||{};for(b in j){if(j.hasOwnProperty(b)){f+=b+"="+j[b]+"&"}}var i="json"+(++a);d[i]=function(l){k(l);d[i]=null;try{delete d[i]}catch(m){}};e(h+f+"callback="+i);return i}return{get:g}}());
 var livefyre_wp_plugin_polled = false;
 
-function formatDate(date_) {
-    var formatted = 'Completed on ';
-    formatted += d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
-    formatted += ' at ' + (d.getHours() > 12 ? d.getHours() - 12 : d.getHours()) + ':' + d.getMinutes() + (d.getHours() > 12 ? 'pm' : 'am');
-    return formatted;
+function updateProgressBarLF(conv_count, convs_processed) {
+    var percentage_complete = 0;
+    if (typeof(conv_count) !== 'number') {
+        conv_count = 0;
+    }
+    if (typeof(convs_processed) !== 'number') {
+        convs_processed = 0;
+    }
+    document.getElementById('fyre-progress-title').innerHTML = convs_processed + '/' + conv_count + ' Conversations';
+    percentage_complete = (100 * convs_processed / conv_count);
+    if (!isNaN(percentage_complete)) {
+        var prog = document.getElementById('fyre-progress-bar');
+        prog.style.width = percentage_complete + '%';
+        prog.style.border = '1px solid #58891c';
+    }
 }
 
 function checkStatusLF(){
@@ -34,47 +43,32 @@ function checkStatusLF(){
         var status = data['status'],
             loc = '?page=livefyre';
 
-        if (status === 'aborted' || status === 'failed') {
-            // Statuses that signal a stopping point in the process.
-            loc += '&status=error';
-            if (data['import_failure'] && data['import_failure']['message']) {
-                loc += '&message=' + data['import_failure']['message'];
-            }
-            // TODO: UNCOMMENT
-            //window.location.href = loc;
-        } else if (status === 'complete') {
-            // TODO: UNCOMMENT
-            //window.location.href = '&status=complete&message=' + formatDate(new Date(data['last_modified']));
-        } else {
-            // Update the process bar.
-            var conv_count = data['conversation_count'],
-                convs_processed = data['conversations_processed_count'],
-                percentage_complete = 0;
-            if (typeof(conv_count) !== 'number') {
-                conv_count = 0;
-            }
-            if (typeof(convs_processed) !== 'number') {
-                convs_processed = 0;
-            }
-            document.getElementById('fyre-progress-title').innerHTML = convs_processed + '/' + conv_count + ' Conversations';
-            percentage_complete = convs_processed / conv_count;
-            if (!isNaN(percentage_complete)) {
-                var prog = document.getElementById('fyre-progress-bar');
-                prog.style.width = percentage_complete + '%';
-                prog.style.border = '1px solid #58891c';
-            }
+        switch(status) {
+            case 'aborted':
+            case 'failed':
+                // Statuses that signal a stopping point in the process.
+                loc += '&status=error';
+                if (data['import_failure'] && data['import_failure']['message']) {
+                    loc += '&message=' + data['import_failure']['message'];
+                }
+                window.location.href = loc;
+                break;
+            
+            default:
+                updateProgressBarLF(data['conversation_count'], data['conversations_processed_count']);
+        }
+        if (status === 'complete') {
+            // 'complete' in this response is equivalent to the 'csv_uploaded' status internally
+            // we'll wait a moment to let the progress bar look pretty
+            setTimeout(function(){
+                // reload the page, showing completed status.
+                window.location.href = window.location.href.split('?')[0] + '?page=livefyre';
+            }, 2500);
         }
         livefyre_wp_plugin_polled=true;
     });
 }
 
-function wordpress_start_ajax() {
-    window.checkWPStatusInterval=setInterval(
-        checkStatusWP, 
-        5000
-    );
-    checkStatusWP();    
-}
 function livefyre_start_ajax() {
     window.checkStatusInterval=setInterval(
         checkStatusLF, 
@@ -86,9 +80,11 @@ function livefyre_start_ajax() {
 
 
 <?php
-if (get_option('livefyre_import_status','') == 'started') {
+$status = get_option('livefyre_import_status','');
+if (!in_array($status, array('', 'error', 'csv_uploaded'))) {
     //only report status of the import
     ?>
+    starting ajax...
     <script type="text/javascript">
         livefyre_start_ajax();
     </script>
@@ -125,24 +121,7 @@ if (get_option('livefyre_import_status','') == 'started') {
                 </div>
             </div>
         </div>
-    <?php
-    } else if ($import_status == 'started') {
-        ?>
-        <div class="fyre-container-base" id="fyre-in-progress">
-            <div class="fyre-container">
-                <div class="fyre-header">
-                    <div class="fyre-status slate"></div>
-                    <span id="fyre-progress-title" class="fyre-title"></span>
-                    <span class="fyre-subtext">
-                        Import your existing WordPress comments so that they show up in Livefyre Comments and in the Livefyre Admin.
-                    </span>
-                    <div class="fyre-progress-bar-container">
-                        <div id="fyre-progress-bar" class="fyre-progress-bar"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php
+        <?php
     } else if ($import_status == 'error') {
         ?>
         <div class="fyre-container-base" id="fyre-failure">
@@ -157,7 +136,7 @@ if (get_option('livefyre_import_status','') == 'started') {
             </div>
         </div>
         <?php
-    } else if ($import_status == 'complete') {
+    } else if ($import_status == 'csv_uploaded') {
         ?>
         <div class="fyre-container-base" id='fyre-success'>
             <div class="fyre-container">
@@ -165,13 +144,29 @@ if (get_option('livefyre_import_status','') == 'started') {
                     <div class="fyre-status green"></div>
                     <span class="fyre-title">Successfully Imported</span>
                     <span class="fyre-subtext">
-                        Completed on 6/26/12 at 5:43pm
+                        <?php echo get_option('livefyre_import_message','') ?>
                     </span>
                 </div>
             </div>
         </div>
         <?php
-    }
+    } else if ($import_status != 'error' ) {
+        ?>
+        <div class="fyre-container-base" id="fyre-in-progress">
+            <div class="fyre-container">
+                <div class="fyre-header">
+                    <div class="fyre-status slate"></div>
+                    <span id="fyre-progress-title" class="fyre-title"></span>
+                    <span class="fyre-subtext">
+                        Import your existing WordPress comments so that they show up in Livefyre Comments and in the Livefyre Admin.
+                    </span>
+                    <div class="fyre-progress-bar-container">
+                        <div id="fyre-progress-bar" class="fyre-progress-bar"></div>
+                    </div>
+                </div>
+            </div>
+        </div><?php 
+    } 
     ?>
 </div>
 <style>
