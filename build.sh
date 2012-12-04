f14719691c59637340e8528a6f133d3c7b2855b3
Nino Walker <nino.walker@gmail.com>
2012-12-04 10:35:50 -0800

    start of v4 dev branch.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-zip -r livefyre-`sed 's/\./_/' version`.zip livefyre-comments/ -x "livefyre-comments/**/.*" -x "livefyre-comments/.*"
+#!/bin/bash
+
+version=`sed 's/\./_/' version`
+archive=livefyre-${version}.zip
+
+zip -r $archive livefyre-comments/ -x "livefyre-comments/**/.*" -x "livefyre-comments/.*"
+
 echo "
-*Made a new zip file for you: livefyre-`sed 's/\./_/' version`.zip"
+*Made a new zip file for you: $archive"
