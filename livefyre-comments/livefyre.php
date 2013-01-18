f9a1712eee7bcc465468a837bd55a607d96c40ac
Mike Soldner <mike@livefyre.com>
2013-01-17 20:10:54 -0800

    Bumped version to 4.0

--- livefyre-comments/livefyre.php
+++ livefyre-comments/livefyre.php
@@ -4,7 +4,7 @@ Plugin Name: Livefyre Realtime Comments
 Plugin URI: http://livefyre.com/wordpress#
 Description: Implements livefyre realtime comments for WordPress
 Author: Livefyre, Inc.
-Version: 3.52
+Version: 4.0.0
 Author URI: http://livefyre.com/
 */
 
