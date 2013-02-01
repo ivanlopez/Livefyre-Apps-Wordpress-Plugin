15fcd16a5e70f3eba0bf770c5cd7e8ab9132cb3a
Mike Soldner <mike@livefyre.com>
2013-01-31 16:42:50 -0800

    Code beautification.

--- livefyre-comments/logger.php
+++ livefyre-comments/logger.php
@@ -8,16 +8,16 @@ class Logger {
 
     function add( $message, $database_flag = false ) {
         if ( WP_DEBUG === true ) {
-            if ( $database_flag ) {
-                global $wpdb;
-                error_log( $message );
-                $wpdb->query("insert into livefyre_debug_blobs (text) values ('". $message . "');");
-            } else if ( is_array( $message ) || is_object( $message ) ) {
-                error_log( print_r( $message, true ) );
-            } else {
-                error_log ( $message );
-            }
+            return;
         }
+        if ( is_array( $message ) || is_object( $message ) ) {
+            $message = print_r( $message );
+        }
+        if ( $database_flag ) {
+            global $wpdb;
+            $wpdb->query("insert into livefyre_debug_blobs (text) values ('". $message . "');");
+        }
+        error_log ( $message );
     }
 }
 
