d0bb94611bcf837abc8dbbafb8f2b7a4c8dbc0e1
Michael Liao <michael@Michaels-MacBook-Pro.local>
2013-03-04 18:12:25 -0800

    Took out some logging messages

--- livefyre-comments/livefyre_core.php
+++ livefyre-comments/livefyre_core.php
@@ -334,6 +334,7 @@ class Livefyre_Sync {
         if ( !is_array( $json_array ) ) {
             $this->schedule_sync( LF_SYNC_LONG_TIMEOUT );
             $error_message = 'Error during do_sync: Invalid response ( not a valid json array ) from sync request to url: ' . $url . ' it responded with: ' . $str_comments;
+            $this->lf_core->Logger->add( "Livefyre: Invalid response ( not a valid json array) from sync request." );
             $this->livefyre_report_error( $error_message );
             return array_merge(
                 $result,
@@ -393,6 +394,8 @@ class Livefyre_Sync {
         $result[ 'last-activity-id' ] = $last_activity_id;
         if ( $last_activity_id ) {
             $this->ext->update_option( 'livefyre_activity_id', $last_activity_id );
+            $last_id_msg = 'Livefyre: Set last activity ID processed to ' . $last_activity_id;
+            $this->lf_core->Logger->add($last_id_msg);
         }
         $this->schedule_sync( $timeout );
         return $result;
@@ -491,7 +494,6 @@ class Livefyre_Sync {
     }
 
     function livefyre_insert_activity( $data ) {
-        $this->lf_core->Logger->add('Livefyre: processing a site sync activity');
         if ( isset( $data[ 'lf_comment_parent' ] ) && $data[ 'lf_comment_parent' ]!= null ) {
             $app_comment_parent = $this->ext->get_app_comment_id( $data[ 'lf_comment_parent' ] );
             if ( $app_comment_parent == null ) {
@@ -514,10 +516,6 @@ class Livefyre_Sync {
             'comment-update',
             'comment-delete'
         );
-        $this->lf_core->Logger->add('Livefyre comment data: ');
-        $this->lf_core->Logger->add($data);
-        $this->lf_core->Logger->add('Processing WP comment ID: ');
-        $this->lf_core->Logger->add($app_comment_id);
         if ( $app_comment_id > '' && in_array( $at, $action_types ) ) {
 
             // update existing comment
@@ -527,27 +525,21 @@ class Livefyre_Sync {
             $mod = count( $at_parts ) > 1 ? $at_parts[ 1 ] : '';
             if ( $action == 'comment-moderate' ) {
                 if ( $mod == 'mod-approve' ) {
-                    $this->lf_core->Logger->add('Livefyre: Site sync approving a comment');
                     $this->ext->update_comment_status( $app_comment_id, 'approve' );
                 } elseif ( $mod == 'mod-hide' && $data[ 'lf_state' ] == 'hidden' ) {
-                    $this->lf_core->Logger->add('Livefyre: Site sync mod-hide (soft delete) move to trash');
                     $this->ext->delete_comment( $app_comment_id );
                 } elseif ( $mod == 'mod-unapprove') {
-                    $this->lf_core->Logger->add('Livefyre: Site sync unapproving a comment');
                     $this->ext->update_comment_status( $app_comment_id, 'hold' );
                 } elseif ( $mod == 'mod-mark-spam' || $mod == 'mod-bozo') {
-                    $this->lf_core->Logger->add('Livefyre: Site sync marking comment as spam');
                     $this->ext->update_comment_status( $app_comment_id, 'spam' );
                 }
             } elseif ( ($action == 'comment-update' || $action == 'comment-add') && isset( $data[ 'comment_content' ] ) && $data[ 'comment_content' ] != '' ) {
                 // even if its supposed to be an "add", when we find the app comment ID, it must be an update
-                $this->lf_core->Logger->add('Livefyre: Site sync comment update');
                 $ret_val = $this->ext->update_comment( $data );
                 if ( $data[ 'lf_state' ] == 'unapproved' ) {
                     $this->ext->update_comment_status( $app_comment_id, 'hold' );
                 }
             } elseif ($action == 'comment-delete') {
-                $this->lf_core->Logger->add('Livefyre: Site sync moving comment to trash');
                 $this->ext->delete_comment( $app_comment_id );
             }
         } elseif ( in_array( $at, array( 'comment-add', 'comment-moderate:mod-approve' ) ) ) {
@@ -556,10 +548,8 @@ class Livefyre_Sync {
                 livefyre_report_error( 'comment_content missing for synched activity id:' . $data[ 'lf_activity_id' ] );
             }
             if ( $data[ 'lf_state' ] != 'deleted' && $data[ 'lf_state' ] != 'hidden' ) {
-                $this->lf_core->Logger->add('Livefyre: Site sync adding new comment');
                 $app_comment_id = $this->ext->insert_comment( $data );
                 if ( $data[ 'lf_state' ] == 'unapproved' ) {
-                    $this->lf_core->Logger->add('Livefyre: Site sync approving new comment');
                     $this->ext->update_comment_status( $app_comment_id, 'unapproved' );
                 }
             }
