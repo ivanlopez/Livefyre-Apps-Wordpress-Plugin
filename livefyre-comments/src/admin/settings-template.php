966cf3595ff229150a057592ccfff99b16f79dd2
Michael Liao <michael@Michaels-MacBook-Pro.local>
2013-08-05 18:48:34 -0700

    Move javascript back because it broke imports, change to import_impl to fix imports

--- livefyre-comments/src/admin/settings-template.php
+++ livefyre-comments/src/admin/settings-template.php
@@ -26,6 +26,78 @@ if ( isset( $_GET['status'] ) ) {
     delete_option( 'livefyre_v3_notify_installed' );
     delete_option( 'livefyre_v3_notify_upgraded' );
 }
+?>
+
+<script type="text/javascript">
+
+//Lightweight JSONP fetcher - www.nonobtrusive.com
+var JSONP=(function(){var a=0,c,f,b,d=this;function e(j){var i=document.createElement("script"),h=false;i.src=j;i.async=true;i.onload=i.onreadystatechange=function(){if(!h&&(!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){h=true;i.onload=i.onreadystatechange=null;if(i&&i.parentNode){i.parentNode.removeChild(i)}}};if(!c){c=document.getElementsByTagName("head")[0]}c.appendChild(i)}function g(h,j,k){f="?";j=j||{};for(b in j){if(j.hasOwnProperty(b)){f+=b+"="+j[b]+"&"}}var i="json"+(++a);d[i]=function(l){k(l);d[i]=null;try{delete d[i]}catch(m){}};e(h+f+"callback="+i);return i}return{get:g}}());
+
+var secondsPassed = 0;
+var stub = "Progress: ";
+
+function checkStatusLF(){
+    JSONP.get( '<?php echo $this->lf_core->quill_url ?>/import/wordpress/<?php echo get_option("livefyre_site_id") ?>/status', {param1:'none'}, function(data){
+        console.log('REPSONSE:', data);
+        var status = data['status'],
+            loc = '?page=livefyre';
+
+        switch(status) {
+            case 'aborted':
+            case 'failed':
+                // Statuses that signal a stopping point in the process.
+                loc += '&status=error';
+                if (data['import_failure'] && data['import_failure']['message']) {
+                    loc += '&message=' + data['import_failure']['message'];
+                }
+                window.location.href = loc;
+                break;
+            
+            default:
+                secondsPassed++;
+                if(secondsPassed <= 20) {
+                    message = "Warming up the engine...";
+                }
+                else if(secondsPassed >= 20 && secondsPassed < 60) {
+                    message = "Starting the move...";
+                }
+                else if(secondsPassed >= 60 && secondsPassed < 30) {
+                    message = "Hang tight, work in progress...";
+                }
+                else if(secondsPassed >= 300 && secondsPassed < 600) {
+                    message = "We're still cranking away!";
+                }
+                else if(secondsPassed >= 600 && secondsPassed < 1800) {
+                    message = "Maybe it's time for a candy bar.";
+                }
+                else if(secondsPassed >= 1800 && secondsPassed < 2700) {
+                    message = 'In the meantime, check out our Facebook page at <a href="http://www.facebook.com/livefyre">facebook.com/livefyre</a>';
+                }
+                else if(secondsPassed >= 2700 && secondsPassed < 3600) {
+                    message = "Boy, you have one popular website...";
+                }
+                else {
+                    message = "Still working here. Thanks for your patience.";
+                }
+                document.getElementById("livefyre-import-text").innerHTML = stub + message;
+        }
+        if (status === 'complete') {
+            window.location.href = window.location.href.split('?')[0] + '?page=livefyre';
+        }
+    });
+}
+
+function livefyre_start_ajax(iv) {
+    window.checkStatusInterval=setInterval(
+        checkStatusLF, 
+        iv
+    );
+    checkStatusLF();
+}
+
+</script>
+
+<?php
 
 if (isset($_GET['hide_import_message'])) {
     update_option( 'livefyre_import_status', 'complete' );
@@ -50,6 +122,16 @@ if ( $import_status == 'csv_uploaded') {
 }
 elseif ( $import_status == 'started' ) {
     $import_status = 'pending';
+}
+
+// Start the animation only if the button was clicked
+if ( $import_status == 'pending' ) {
+    // Only report status of the import
+    ?>
+    <script type="text/javascript">
+        livefyre_start_ajax(1000);
+    </script>
+    <?php
 }
 
 $deactivated_time = get_option( 'livefyre_deactivated', ': '.time() );
@@ -418,72 +500,8 @@ $upgrade_status = get_option( 'livefyre_backend_upgrade', false );
     <?php echo file_get_contents( dirname( __FILE__ ) . '/settings-template.css' )  ?>
 </style>
 
+
 <script type="text/javascript">
-
-//Lightweight JSONP fetcher - www.nonobtrusive.com
-var JSONP=(function(){var a=0,c,f,b,d=this;function e(j){var i=document.createElement("script"),h=false;i.src=j;i.async=true;i.onload=i.onreadystatechange=function(){if(!h&&(!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){h=true;i.onload=i.onreadystatechange=null;if(i&&i.parentNode){i.parentNode.removeChild(i)}}};if(!c){c=document.getElementsByTagName("head")[0]}c.appendChild(i)}function g(h,j,k){f="?";j=j||{};for(b in j){if(j.hasOwnProperty(b)){f+=b+"="+j[b]+"&"}}var i="json"+(++a);d[i]=function(l){k(l);d[i]=null;try{delete d[i]}catch(m){}};e(h+f+"callback="+i);return i}return{get:g}}());
-
-var secondsPassed = 0;
-var stub = "Progress: ";
-
-function checkStatusLF(){
-    JSONP.get( '<?php echo $this->lf_core->quill_url ?>/import/wordpress/<?php echo get_option("livefyre_site_id") ?>/status', {param1:'none'}, function(data){
-        console.log('REPSONSE:', data);
-        var status = data['status'],
-            loc = '?page=livefyre';
-
-        switch(status) {
-            case 'aborted':
-            case 'failed':
-                // Statuses that signal a stopping point in the process.
-                loc += '&status=error';
-                if (data['import_failure'] && data['import_failure']['message']) {
-                    loc += '&message=' + data['import_failure']['message'];
-                }
-                window.location.href = loc;
-                break;
-            
-            default:
-                secondsPassed++;
-                if(secondsPassed <= 20) {
-                    message = "Warming up the engine...";
-                }
-                else if(secondsPassed >= 20 && secondsPassed < 60) {
-                    message = "Starting the move...";
-                }
-                else if(secondsPassed >= 60 && secondsPassed < 30) {
-                    message = "Hang tight, work in progress...";
-                }
-                else if(secondsPassed >= 300 && secondsPassed < 600) {
-                    message = "We're still cranking away!";
-                }
-                else if(secondsPassed >= 600 && secondsPassed < 1800) {
-                    message = "Maybe it's time for a candy bar.";
-                }
-                else if(secondsPassed >= 1800 && secondsPassed < 2700) {
-                    message = 'In the meantime, check out our Facebook page at <a href="http://www.facebook.com/livefyre">facebook.com/livefyre</a>';
-                }
-                else if(secondsPassed >= 2700 && secondsPassed < 3600) {
-                    message = "Boy, you have one popular website...";
-                }
-                else {
-                    message = "Still working here. Thanks for your patience.";
-                }
-                document.getElementById("livefyre-import-text").innerHTML = stub + message;
-        }
-        if (status === 'complete') {
-            window.location.href = window.location.href.split('?')[0] + '?page=livefyre';
-        }
-    });
-}
-
-function livefyre_start_ajax(iv) {
-    window.checkStatusInterval=setInterval(
-        checkStatusLF, 
-        iv
-    );
-    checkStatusLF();
-}
 
 function toggler(section) {
     var info = document.getElementById(section + 'information');
@@ -518,15 +536,3 @@ if (hide_import_button != null) {
 }
 
 </script>
-
-<?php
-// Start the animation only if the button was clicked
-if ( $import_status == 'pending' ) {
-    // Only report status of the import
-    ?>
-    <script type="text/javascript">
-        livefyre_start_ajax(1000);
-    </script>
-    <?php
-}
-?>
